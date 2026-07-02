8a9033ecf1cf1169aa000578bd80cb60eca4ed56
Hus <binaeihossein24@gmail.com>
2026-07-02 12:11:59 +0330

    Core updated

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.get_time, name='get_time'),
-    path('elapsed/<str:pk>', views.elapsed_view, name='elapsed'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,22 +1,4 @@
 from django.shortcuts import render
-from core.utils import jalali_now
-from django.http import HttpResponseBadRequest
-from core.services.elapsed import get_elapsed
 
 def get_time(request):
-    now = jalali_now()
-    formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
-    context = {'time': formatted_now}
-    
-    return render(request, 'core/index.html', context)
-    
-
-def elapsed_view(request, pk):
-    now = jalali_now()
-    value = get_elapsed(pk, now)
-
-    if not value:
-        return HttpResponseBadRequest("Invalid key")
-    context = {'value': value}
-
-    return render(request, 'core/elapsed.html', context)
+    return render(request, 'core/index.html')
