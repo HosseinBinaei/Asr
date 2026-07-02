cf09d04959ebb667bdcf18343bb46809e50671b6
Hus <Hus@gmail.com>
2026-07-02 10:48:38 +0330

    .

--- Asr/settings.py
+++ Asr/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+lzofkk$0s1ayuhh%3irp!7-!dy5xt#9x^zh*06lmhqhc8!7(%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
